e33d0c4ee251c93549c585c94d284469f777a5c2
moarse <ivan.don4enko@gmail.com>
2024-04-10 14:32:06 +0300

    template init

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0Mjc3OA/spectral.js?branch_slug=master&token=c3029d8a-f0a4-40f6-b988-6651badbd35d";
+export default { extends: ruleset };
